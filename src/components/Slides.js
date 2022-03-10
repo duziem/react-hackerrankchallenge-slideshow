18a0c1ec56d0f87b964a9cf5a8d758fb17ead3c4
duziem <frenzyokeke@gmail.com>
2022-03-10 20:50:13 +0000

    edit

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
+//let copyIndex= 0;
 function Slides({slides}) {
+    const [currentSlide, setCurrentSlide]= useState(slides[0])
+    const [index, setIndex]= useState(0)
+
+    const handleClick= (e)=>{
+        //index++
+        if(e.target.data-testid.includes('next')){
+            let copyIndex= index;
+            if(index === slides.length - 1){
+                copyIndex = 0;
+            }
+            copyIndex += 1;
+            setIndex(copyIndex);
+        }
+        if(e.target.data-testid.includes('prev')){
+            let copyIndex= index;
+            if(index === 0){
+                copyIndex = slides.length - 1
+            }
+            copyIndex -= 1;
+            setIndex(copyIndex);
+        }
+        if(e.target.data-testid.includes('restart')){
+            setIndex(0);
+        }
+        
+    }
+
+    useEffect(()=>{
+        setCurrentSlide(slides[index])
+    }, [index])
+
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick= {handleClick} disabled= {index === 0} >Restart</button>
+                <button data-testid="button-prev" className="small" onClick= {handleClick} disabled= {index === 0}>Prev</button>
+                <button data-testid="button-next" className="small" onClick= {handleClick} disabled= {index === slides.length - 1}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{currentSlide.title}</h1>
+                <p data-testid="text">{currentSlide.text}</p>
             </div>
         </div>
     );
